3ba18cb6b3f2e02a46a72dc9327eb9419ef3d06b
siyeon3 <88600419+siyeon3@users.noreply.github.com>
2023-03-12 14:55:56 +0900

    Add files via upload

---
+++ ch22/src/main/java/com/fastcampus/ch22/Car.java
@@ -0,0 +1,6 @@
+package com.fastcampus.ch22;
+
+public class Car    { 
+	private String color = "red"; 
+	public String getColor() { return color; }
+}
---
+++ ch22/src/main/java/com/fastcampus/ch22/Person.java
@@ -0,0 +1,6 @@
+package com.fastcampus.ch22;
+
+public class Person { 
+	private Car car = new Car(); 
+	public  Car getCar() { return car; }
+}  
